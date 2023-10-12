3de245bc952ea88b5f607423c89ac78fa0c254b8
Gabriela Constante <87451288+gabrielaconstante@users.noreply.github.com>
2023-10-11 22:20:09 -0300

    Delete Desktop/diabetes-n1-gabrielaconstante-main directory

--- Desktop/diabetes-n1-gabrielaconstante-main/lib/state.dart
+++
@@ -1,7 +0,0 @@
-class State {
-  String text, choice1;
-  String? choice2;
-
-  // State(this.text, this.choice1, this.choice2);
-  State({required this.text, required this.choice1, this.choice2});
-}
