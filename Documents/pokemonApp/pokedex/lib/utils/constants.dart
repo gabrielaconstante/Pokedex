dbfb154158b0c21205cf4db47f7ac279676cf97b
gabrielaconstante <gabrielaconstante04@gmail.com>
2023-10-11 22:16:47 -0300

    First commit

---
+++ Documents/pokemonApp/pokedex/lib/utils/constants.dart
@@ -0,0 +1,3 @@
+const pokeApiUrl = 'https://pokeapi.co/api/v2/pokemon';
+const pokemonImgUrl = 'https://raw.githubusercontent.com/PokeAPI/'
+    'sprites/master/sprites/pokemon/other/official-artwork';
